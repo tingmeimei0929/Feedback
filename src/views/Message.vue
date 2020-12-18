ece074151384cdad20b8fec7616900b1ec95b6b4
qingting.jiao <1259043979@qq.com>
2020-12-18 15:06:19 +0800

    scss调整

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -26,7 +26,7 @@
                 </el-upload>
             </el-form-item>
             <el-form-item>
-                <el-button  type="primary" @click="onSubmit" style="margin-left:100px;">提交</el-button>
+                <el-button  type="primary" @click="onSubmit">提交</el-button>
                 <el-button>取消</el-button>
             </el-form-item>
         </el-form>
